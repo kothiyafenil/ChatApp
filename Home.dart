435b4d26a1c17294c2735a387cd2044ed29e4f3c
kothiyafenil <128579029+kothiyafenil@users.noreply.github.com>
2023-03-30 17:54:07 +0530

    Add files via upload

--- Home.dart
+++ Home.dart
@@ -2,11 +2,14 @@ import 'dart:developer';
 import 'dart:ffi';
 
 import 'package:chat/Screen/Chating.dart';
+import 'package:chat/Screen/CreateGroup.dart';
+import 'package:chat/Screen/Groupchat.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:contacts_service/contacts_service.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 class Home extends StatefulWidget {
@@ -50,7 +53,7 @@ class _HomeState extends State<Home> with WidgetsBindingObserver {
     super.initState();
     WidgetsBinding.instance.addObserver(this);
     setstatus("Online");
-    getPermission();
+    // getPermission();
   }
 
   void setstatus(String status) {
@@ -72,73 +75,107 @@ class _HomeState extends State<Home> with WidgetsBindingObserver {
 
   void getPermission() async {
     if (await Permission.contacts.isGranted) {
-      fetchdata();
+      // fetchdata();
 // catch data..
     } else {
       await Permission.contacts.request();
     }
   }
 
-  List<Contact> contacts = [];
+  // List<Contact> contacts = [];
 
-  void fetchdata() async {
-    contacts = await ContactsService.getContacts();
-    // print(contacts[0].phones![0].value.toString());
-  }
+  // void fetchdata() async {
+  //   contacts = await ContactsService.getContacts();
+  // }
 
   // contact na phone number and apa data set karava ena phone number same hova jovi
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.blueGrey,
-            centerTitle: true,
-            title: Text(
-              FirebaseAuth.instance.currentUser!.displayName.toString(),
-              style: const TextStyle(color: Colors.white, fontSize: 25, fontStyle: FontStyle.italic),
-            ),
-          ),
-          body: StreamBuilder<QuerySnapshot>(
-              stream: FirebaseFirestore.instance.collection("user").snapshots(),
-              builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-                if (snapshot.hasData) {
-                  return Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 8),
-                    child: ListView.builder(
-                      itemCount: snapshot.data!.docs.length,
-                      itemBuilder: (BuildContext context, int index) {
-                        return ListTile(
-                          onTap: () {
-                            String id = ChatRoomId(FirebaseAuth.instance.currentUser!.displayName.toString(), snapshot.data!.docs[index]["name"]);
-                            String userId = snapshot.data!.docs[index]["userId"];
-                            log(id);
-                            String name = snapshot.data!.docs[index]["name"];
-                            Navigator.of(context).push(
-                              MaterialPageRoute(
-                                builder: (_) => Chating(
-                                  userMap: name.toString(),
-                                  id: id,
-                                  userId: userId,
-                                ),
-                              ),
-                            );
-                          },
-                          leading: const Icon(
-                            Icons.account_circle_rounded,
-                            size: 50,
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.blueGrey,
+          child: const Icon(Icons.group),
+          onPressed: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => const Groupchat(),
+              ),
+            );
+          },
+        ),
+        appBar: AppBar(
+          actions: [
+            PopupMenuButton(
+              itemBuilder: (context) {
+                return [
+                  PopupMenuItem(
+                    value: 0,
+                    child: InkWell(
+                      onTap: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) => const CreateGroup(),
                           ),
-                          title: Text(snapshot.data!.docs[index]["name"].toString()),
-                          subtitle: Text(snapshot.data!.docs[index]["email"]),
-                          trailing: const Icon(Icons.message),
                         );
                       },
+                      child: const Text("Create a group"),
                     ),
-                  );
-                } else {
-                  return const Center(child: CircularProgressIndicator());
-                }
-              })),
+                  ),
+                ];
+              },
+            )
+          ],
+          backgroundColor: Colors.blueGrey,
+          centerTitle: true,
+          title: Text(
+            FirebaseAuth.instance.currentUser!.displayName.toString(),
+            style: const TextStyle(color: Colors.white, fontSize: 25, fontStyle: FontStyle.italic),
+          ),
+        ),
+        body: StreamBuilder<QuerySnapshot>(
+          stream: FirebaseFirestore.instance.collection("user").snapshots(),
+          builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (snapshot.hasData) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8),
+                child: ListView.builder(
+                  itemCount: snapshot.data!.docs.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return ListTile(
+                      onTap: () {
+                        String id = ChatRoomId(FirebaseAuth.instance.currentUser!.displayName.toString(), snapshot.data!.docs[index]["name"]);
+                        String userId = snapshot.data!.docs[index]["userId"];
+                        log(id);
+                        String name = snapshot.data!.docs[index]["name"];
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (_) => Chating(
+                              userMap: name.toString(),
+                              id: id,
+                              userId: userId,
+                            ),
+                          ),
+                        );
+                      },
+                      leading: const Icon(
+                        Icons.account_circle_rounded,
+                        size: 50,
+                      ),
+                      title: Text(snapshot.data!.docs[index]["name"].toString()),
+                      subtitle: Text(snapshot.data!.docs[index]["email"]),
+                      trailing: const Icon(Icons.message),
+                    );
+                  },
+                ),
+              );
+            } else {
+              return const Center(child: CircularProgressIndicator());
+            }
+          },
+        ),
+      ),
     );
   }
 }
